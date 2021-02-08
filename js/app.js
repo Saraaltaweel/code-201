0e0f9f1352e7d0ffa75fb776d6e6d282a00700d2
Saraaltaweel <s.bata97.st@gmail.com>
2021-02-08 15:45:47 +0200

    everythings done

---
+++ js/app.js
@@ -0,0 +1,59 @@
+let yourName = prompt('what your name?');
+alert(`welcome ${yourName} in my page`);
+
+
+
+let favMovie = 'hoydat';
+let favMovie = prompt('what is your fav character?');
+
+
+if(favMovie.toLowerCase() === myCharacter){
+  alert('that funy');
+}else {
+  alert('okay');
+}
+console.log(favMovie.toUpperCase()); 
+console.log(favMovie);
+
+
+let yourName = prompt('does your name consist of more than four letters?');
+if (yourName  ==='yes' || yourName  ==='y'){
+alert('wow same to me')
+} else if(yourName  ==='no' || yourName ==='n'){
+  alert('you are different to me')
+}
+console.log(yourName);
+
+let youHungry = prompt('Are you hungry?');
+if (youHungry ==='yes' || youHungry ==='y'){
+alert('go to eat')
+} else if(youHungry ==='no' || youHungry ==='n'){
+  alert('go to drink')
+}
+console.log(youHungry );
+
+let Movie = prompt('have you ever watch movie?');
+if (Movie ==='yes' || Movie ==='y'){
+alert('thats exciting')
+} else if(Movie ==='no' || Movie ==='n'){
+  alert('you are boring')
+}
+console.log(Movie);
+
+let tired = prompt('are you tired?');
+if (tired ==='yes' || tired ==='y'){
+alert('take a brake')
+} else if(tired ==='no' || tired ==='n'){
+  alert('complete your work')
+}
+console.log(tired);
+
+let traval = prompt('have you ever traval?');
+if (traval ==='yes' || traval ==='y'){
+alert('thats great')
+} else if(traval ==='no' || traval ==='n'){
+  alert('no problem')
+}
+console.log(traval);
+
+alert(yourName);
